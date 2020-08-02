aef7f34ee15846c3d1b7c60a464766d7f3e7d97a
gasampaiosouza <gasampaio.souza@gmail.com>
2020-08-02 18:30:14 -0300

    :apple: initial song-list commit

---
+++ songs.py
@@ -0,0 +1,8 @@
+# code only works with the url that is after yt.com/watch?v=
+
+# for example: https://www.youtube.com/watch?v=68ugkg9RePc
+# valid url: 68ugkg9RePc
+
+# valid list example = ['8mCCMhuKEYw', 'PMivT7MJ41M', 'bnVUHWCynig', 'CvBfHwUxHIk']
+
+SONGS_LIST = ['8mCCMhuKEYw', 'PMivT7MJ41M', 'bnVUHWCynig', 'CvBfHwUxHIk']
